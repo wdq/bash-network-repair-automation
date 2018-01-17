c2786a3d277554fd44e53317f0cb7b77c41da7f3
William Quade <liam@quade.co>
2018-01-17 11:11:52 -0600

    Update

--- network_check.sh
+++ network_check.sh
@@ -21,8 +21,8 @@
 # Let's clear the screen
 clear
 
-# Write here the gateway / website you want to check to declare if network is working or not
-gateway_ip='www.google.com'
+# Smalldata VPN server gateway IP
+gateway_ip='172.29.0.1'
 
 # Specify the path of a txt file where the network failures count will be held
 network_check_tries_file='/home/pi/scripts/network_check/network_check_tries.txt'
@@ -44,18 +44,26 @@ fi
 # If network test failed more than 5 times (you can change this value to whatever you prefer)
 if [ $network_check_tries -gt 5 ]; then
 echo "Network was not working for the previous $network_check_tries checks."
-# Time to restart wlan0
-    echo "Restarting wlan0"
-    /sbin/ifdown 'wlan0'
+# Time to restart VPN and Fona
+    echo "Restarting fona"
+    /sbin/ifdown 'fona'
     sleep 5
-    /sbin/ifup --force 'wlan0'
+    /sbin/ifup --force 'fona'
+    sleep 60
+    systemctl restart openvpn@client
     sleep 30
-# Then we check again if restarting wlan0 fixed the issue, if not we reboot as last resort
+# Then we check again if restarting VPN and Fona fixed the issue, if not we reboot as last resort (reboot is really a full power cycle)
     host_status=$(fping $gateway_ip)
     if [[ $host_status == *"alive"* ]]
     then
         echo "Network is working correctly" && echo 0 > $network_check_tries_file
     else
-        echo "Network is down..." && echo 0 > $network_check_tries_file && reboot
+        echo "Network is down..." && echo 0 > $network_check_tries_file
+        /home/pi/src/firmware-smalldata/iface_firmware/gatewayOff
+        timestamp=$(date +%s)
+        waketime="$((timestamp + 60))"
+        /home/pi/src/firmware-smalldata/iface_firmware/piWakeup "$waketime"
+        /home/pi/src/firmware-smalldata/iface_firmware/piOff 30
+        /sbin/halt
     fi
 fi
